bd1c87c6287090317204d3e57d4964f639da4fa2
kaihsun <kaihsun@anyscale.com>
2025-04-07 00:52:37 +0000

    only register the callback for in-actor objects
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- src/ray/core_worker/task_manager.cc
+++ src/ray/core_worker/task_manager.cc
@@ -485,24 +485,29 @@ bool TaskManager::HandleTaskReturn(const ObjectID &object_id,
     } else {
       direct_return = in_memory_store_.Put(object, object_id);
     }
-  }
 
-  reference_counter_.AddObjectOutOfScopeOrFreedCallback(
-      object_id, [this](const ObjectID &object_id) {
-        // TODO: Not every object needs to add this callback.
-        // Only the objects that are pinned in the actor need to add this callback.
-        RAY_LOG(INFO) << "New callback is called, object_id: " << object_id;
-        auto actor_id = ObjectID::ToActorID(object_id);
-        auto rpc_client = get_actor_rpc_client_callback_(actor_id);
-        auto request = rpc::CleanUpInActorObjectRequest();
-        request.set_object_id(object_id.Binary());
-        RAY_LOG(INFO) << "CleanUpInActorObject " << object_id
-                      << " binary: " << object_id.Binary();
-        rpc_client->CleanUpInActorObject(
-            request, [](Status status, const rpc::CleanUpInActorObjectReply &reply) {
-              RAY_CHECK(status.ok());
-            });
-      });
+    if (object.IsInActorError()) {
+      RAY_LOG(INFO) << "Object is stored in actor object store, registering cleanup "
+                       "callback for object ID: "
+                    << object_id;
+      reference_counter_.AddObjectOutOfScopeOrFreedCallback(
+          object_id, [this](const ObjectID &object_id) {
+            // TODO: Not every object needs to add this callback.
+            // Only the objects that are pinned in the actor need to add this callback.
+            RAY_LOG(INFO) << "New callback is called, object_id: " << object_id;
+            auto actor_id = ObjectID::ToActorID(object_id);
+            auto rpc_client = get_actor_rpc_client_callback_(actor_id);
+            auto request = rpc::CleanUpInActorObjectRequest();
+            request.set_object_id(object_id.Binary());
+            RAY_LOG(INFO) << "CleanUpInActorObject " << object_id
+                          << " binary: " << object_id.Binary();
+            rpc_client->CleanUpInActorObject(
+                request, [](Status status, const rpc::CleanUpInActorObjectReply &reply) {
+                  RAY_CHECK(status.ok());
+                });
+          });
+    }
+  }
 
   rpc::Address owner_address;
   if (reference_counter_.GetOwner(object_id, &owner_address) && !nested_refs.empty()) {
